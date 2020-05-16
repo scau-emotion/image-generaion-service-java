890f65b4179b46097caecbc794dc8d46edfeead9
lhc328 <2367542852@qq.com>
2020-05-16 15:21:23 +0800

    Split generate function

--- src/main/java/com/mini/wechat/emotiongenerateservice/service/EmotionGrpcService.java
+++ src/main/java/com/mini/wechat/emotiongenerateservice/service/EmotionGrpcService.java
@@ -50,12 +50,66 @@ public class EmotionGrpcService extends EmotionServiceGrpc.EmotionServiceImplBas
         Graphics2D graphics2D = wordImage.createGraphics();
         // 字体大小
         Font font = new Font("微软雅黑", Font.PLAIN, 30);
-
         FontMetrics fm = graphics2D.getFontMetrics(font);
         int fontWidth = fm.stringWidth(content);
-
-        List<String> strList = new ArrayList<>();
         int deHeight = 50;
+
+        // 1. 分割换行文字
+        List<String> strList = getWordList(fontWidth, srcWidth, content, fm);
+        // 需要换行时，修改图片大小
+        if (strList.size() > 1) {
+            // 总字符串长度是文本宽度的多少倍;
+            deHeight = strList.size() * 45;
+            wordImage = new BufferedImage(srcWidth, deHeight, BufferedImage.TYPE_INT_RGB);
+            graphics2D = wordImage.createGraphics();
+        }
+        // 2. 画出文字
+        drawWordImage(graphics2D, srcWidth, deHeight, strList, font);
+        // 3. 输出图片
+        String path = "D:/" + new Date().getTime() + "-1.png";
+        ImageIO.write(wordImage, "png", new FileOutputStream(path));
+        // 4. 拼接图片
+        stitchImage(image1, wordImage, srcWidth, srcHeight, deHeight);
+        return "ok";
+    }
+
+    private void stitchImage(Image templateImage, BufferedImage wordImage, int srcWidth, int srcHeight,
+                             int deHeight) throws Exception {
+        BufferedImage resImage = new BufferedImage(srcWidth, srcHeight + deHeight, BufferedImage.TYPE_INT_RGB);
+        int[] imageArrayOne = new int[srcWidth * srcHeight];
+        imageArrayOne = ((BufferedImage) templateImage).getRGB(0, 0, srcWidth, srcHeight, imageArrayOne, 0, srcWidth);
+        int[] imageArrayTwo = new int[srcWidth * deHeight];
+        imageArrayTwo = wordImage.getRGB(0,0, srcWidth, deHeight, imageArrayTwo, 0, srcWidth);
+        resImage.setRGB(0, 0, srcWidth, srcHeight, imageArrayOne, 0, srcWidth);
+        resImage.setRGB(0, srcHeight, srcWidth, deHeight, imageArrayTwo, 0, srcWidth);
+        String path = "D:/" + new Date().getTime() + "-2.png";
+        ImageIO.write(resImage, "png", new FileOutputStream(path));
+    }
+
+    private void drawWordImage(Graphics2D graphics2D, int srcWidth, int deHeight, List<String> strList,
+                               Font font) {
+        graphics2D.setColor(Color.BLUE);
+        graphics2D.fillRect(0, 0, srcWidth, deHeight);
+        graphics2D.setFont(font);
+        graphics2D.setColor(new Color(188,188, 188));
+        FontMetrics fm = graphics2D.getFontMetrics(font);
+        // 离左边   文字底离顶部
+        for (int i = 0; i < strList.size(); i++) {
+            if (i == strList.size() - 1) {
+                // 最后一行需要居中，前面太长的直接铺满
+                String lastLine = strList.get(i);
+                int lastWidth = fm.stringWidth(lastLine);
+                graphics2D.drawString(strList.get(i), (srcWidth - lastWidth) >> 1, (i + 1) * 38);
+            } else {
+                graphics2D.drawString(strList.get(i), 10, (i + 1) * 38);
+            }
+        }
+    }
+
+    // 文字换行
+    private List<String> getWordList(int fontWidth, int srcWidth, String content, FontMetrics fm) {
+        List<String> strList = new ArrayList<>();
+
         if (fontWidth <= srcWidth - 10 ) {
             strList.add(content);
         } else {
@@ -63,8 +117,8 @@ public class EmotionGrpcService extends EmotionServiceGrpc.EmotionServiceImplBas
             int text_width = srcWidth - 20;
             // 每行字数
             double bs = fontWidth * 1.0 / text_width;
-            int lineCharCount = (int)Math.ceil(content.length() / bs);
-            for (int begin = 0; begin < content.length();) {
+            int lineCharCount = (int) Math.ceil(content.length() / bs);
+            for (int begin = 0; begin < content.length(); ) {
                 int end = begin + lineCharCount;
                 end = Math.min(end, content.length());
                 String lineStr = content.substring(begin, end);
@@ -80,40 +134,7 @@ public class EmotionGrpcService extends EmotionServiceGrpc.EmotionServiceImplBas
                 strList.add(lineStr);
                 begin = end;
             }
-            // 总字符串长度是文本宽度的多少倍;
-            deHeight = (strList.size() + 1) * 45;
-            wordImage = new BufferedImage(srcWidth, deHeight, BufferedImage.TYPE_INT_RGB);
-            graphics2D = wordImage.createGraphics();
         }
-
-        graphics2D.setColor(Color.BLUE);
-        graphics2D.fillRect(0, 0, srcWidth, deHeight);
-        graphics2D.setFont(font);
-        graphics2D.setColor(new Color(188,188, 188));
-        // 离左边   文字底离顶部
-        for (int i = 0; i < strList.size(); i++) {
-            if (i == strList.size() - 1) {
-                // 最后一行需要居中，前面太长的直接铺满
-                String lastLine = strList.get(i);
-                int lastWidth = fm.stringWidth(lastLine);
-                graphics2D.drawString(strList.get(i), (srcWidth - lastWidth) >> 1, (i + 1) * 38);
-            } else {
-                graphics2D.drawString(strList.get(i), 10, (i + 1) * 38);
-            }
-        }
-
-        String path = "D:/" + new Date().getTime() + "-1.png";
-        ImageIO.write(wordImage, "png", new FileOutputStream(path));
-
-        BufferedImage resImage = new BufferedImage(srcWidth, srcHeight + deHeight, BufferedImage.TYPE_INT_RGB);
-        int[] imageArrayOne = new int[srcWidth * srcHeight];
-        imageArrayOne = ((BufferedImage) image1).getRGB(0, 0, srcWidth, srcHeight, imageArrayOne, 0, srcWidth);
-        int[] imageArrayTwo = new int[srcWidth * deHeight];
-        imageArrayTwo = wordImage.getRGB(0,0, srcWidth, deHeight, imageArrayTwo, 0, srcWidth);
-        resImage.setRGB(0, 0, srcWidth, srcHeight, imageArrayOne, 0, srcWidth);
-        resImage.setRGB(0, srcHeight, srcWidth, deHeight, imageArrayTwo, 0, srcWidth);
-        path = "D:/" + new Date().getTime() + "-2.png";
-        ImageIO.write(resImage, "png", new FileOutputStream(path));
-        return "ok";
+        return strList;
     }
 }
